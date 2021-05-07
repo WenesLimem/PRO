4b3b9b212e6b78a734cb41092a5dbecd45e986e9
corentin.zeller <contact@corentin-zeller.ch>
2021-05-07 18:30:20 +0200

    Edition de recette, saving in db

--- EkoRestaurant/EkoRestaurant/Services/RecipesService.cs
+++ EkoRestaurant/EkoRestaurant/Services/RecipesService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using EkoRestaurant.Data;
 using EkoRestaurant.Services.Abstractions;
+using Microsoft.EntityFrameworkCore;
 
 namespace EkoRestaurant.Services
 {
@@ -11,6 +12,31 @@ namespace EkoRestaurant.Services
     {
         public RecipesService(ApplicationDbContext dbContext) : base(dbContext)
         {
+        }
+
+
+        public Recipe GetByIdWithIngredients(int id)
+        {
+            return _dbContext.Set<Recipe>()
+                .Include(x => x.IngredientQuantities)
+                    .ThenInclude(y => y.Ingredient)
+                .FirstOrDefault(e => e.Id == id);
+        }
+
+        public IEnumerable<Recipe> FilterWithIngredients()
+        {
+            return _dbContext.Set<Recipe>()
+                .Include(x => x.IngredientQuantities)
+                    .ThenInclude(y =>y.Ingredient);
+        }
+
+
+        public IEnumerable<Recipe> FilterWithIngredients(Func<Recipe, bool> predicate)
+        {
+            return _dbContext.Set<Recipe>()
+                .Include(x => x.IngredientQuantities)
+                .ThenInclude(y => y.Ingredient)
+                .Where(predicate);
         }
     }
 }
