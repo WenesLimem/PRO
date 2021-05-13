550e17296cfb0795936fb7f1bffd9fc322719c06
wenes <wanous19@gmail.com>
2021-05-13 11:30:16 +0200

    [TOREVIEW] Liste des courses working .
     Missing Selecting liste [will be added this afternoon]

---
+++ EkoRestaurant/EkoRestaurant/Migrations/20210513092439_SecondMig.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EkoRestaurant.Migrations
+{
+    public partial class SecondMig : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Recipes_RecipeCategories_CategoryId",
+                table: "Recipes");
+
+            migrationBuilder.DropTable(
+                name: "RecipeIngredientQuantity");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Recipes_CategoryId",
+                table: "Recipes");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                table: "Recipes");
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "AspNetUsers");
+
+            migrationBuilder.CreateTable(
+                name: "ListeDesCourses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CompletionDateTime = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    CreationDateTime = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IsCompleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ListeDesCourses", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ListeDesCoursesElements",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IngredientId = table.Column<int>(type: "int", nullable: true),
+                    ListeDesCoursesId = table.Column<int>(type: "int", nullable: true),
+                    Quantity = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ListeDesCoursesElements", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ListeDesCoursesElements_Ingredients_IngredientId",
+                        column: x => x.IngredientId,
+                        principalTable: "Ingredients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_ListeDesCoursesElements_ListeDesCourses_ListeDesCoursesId",
+                        column: x => x.ListeDesCoursesId,
+                        principalTable: "ListeDesCourses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ListeDesCoursesElements_IngredientId",
+                table: "ListeDesCoursesElements",
+                column: "IngredientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ListeDesCoursesElements_ListeDesCoursesId",
+                table: "ListeDesCoursesElements",
+                column: "ListeDesCoursesId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ListeDesCoursesElements");
+
+            migrationBuilder.DropTable(
+                name: "ListeDesCourses");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryId",
+                table: "Recipes",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "RecipeIngredientQuantity",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IngredientId = table.Column<int>(type: "int", nullable: true),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    RecipeId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RecipeIngredientQuantity", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_RecipeIngredientQuantity_Ingredients_IngredientId",
+                        column: x => x.IngredientId,
+                        principalTable: "Ingredients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_RecipeIngredientQuantity_Recipes_RecipeId",
+                        column: x => x.RecipeId,
+                        principalTable: "Recipes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Recipes_CategoryId",
+                table: "Recipes",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RecipeIngredientQuantity_IngredientId",
+                table: "RecipeIngredientQuantity",
+                column: "IngredientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RecipeIngredientQuantity_RecipeId",
+                table: "RecipeIngredientQuantity",
+                column: "RecipeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Recipes_RecipeCategories_CategoryId",
+                table: "Recipes",
+                column: "CategoryId",
+                principalTable: "RecipeCategories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
